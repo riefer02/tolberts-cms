4337044bb40a33f826f40294909ec3835cf1ffea
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2021-11-26 19:57:21 +0000

    custom-post-type-ui plugin + lib/vendor-acf.php

---
+++ themes/twentytwentyone/lib/vendor-acf.php
@@ -0,0 +1,12 @@
+add_filter( 'register_post_type_args', function( $args, $post_type ) {
+
+  // Change this to the post type you are adding support for
+  if ( 'event' === $post_type ) {
+    $args['show_in_graphql'] = true;
+    $args['graphql_single_name'] = 'event';
+    $args['graphql_plural_name'] = 'events';
+  }
+
+  return $args;
+
+}, 10, 2 );
