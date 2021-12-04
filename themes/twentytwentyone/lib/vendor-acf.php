fa1f5a970b7e77576cdeee3128dc7dbfaff8ddc5
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-04 09:04:00 -0600

    add bandmate post type to WPGraphQL

--- themes/twentytwentyone/lib/vendor-acf.php
+++ themes/twentytwentyone/lib/vendor-acf.php
@@ -1,11 +1,17 @@
 <?php
 
 add_filter('register_post_type_args', function ($args, $post_type) {
-  // Change this to the post type you are adding support for
+    // Change this to the post type you are adding support for
     if ('event' === $post_type) {
         $args['show_in_graphql'] = true;
         $args['graphql_single_name'] = 'event';
         $args['graphql_plural_name'] = 'events';
+    }
+
+    if ('bandmate' === $post_type) {
+        $args['show_in_graphql'] = true;
+        $args['graphql_single_name'] = 'bandmate';
+        $args['graphql_plural_name'] = 'bandmates';
     }
 
     return $args;
