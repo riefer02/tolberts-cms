b688f3693ae8c60c565092f14320acc162f21aff
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-18 11:03:08 -0600

    remove editor event post type v2

--- themes/twentytwentyone/lib/vendor-wordpress.php
+++ themes/twentytwentyone/lib/vendor-wordpress.php
@@ -2,16 +2,16 @@
 /*
  * Removes some menus by page.
  */
-// function wpdocs_remove_menus() {
-//     remove_menu_page('index.php');                  //Dashboard
-//   remove_menu_page('jetpack');                    //Jetpack*
-//   remove_menu_page('edit.php');                   //Posts
-//   remove_menu_page('edit.php?post_type=page');    //Pages
-//   remove_menu_page('edit-comments.php');          //Comments
-//   remove_menu_page('themes.php');                 //Appearance
-//   remove_menu_page('plugins.php');                //Plugins
-//   remove_menu_page('users.php');                  //Users
-//   remove_menu_page('tools.php');                  //Tools
-//   remove_menu_page('options-general.php');        //Settings
-// }
-// add_action('admin_menu', 'wpdocs_remove_menus');
+function wpdocs_remove_menus() {
+    remove_menu_page('index.php');                  //Dashboard
+  remove_menu_page('jetpack');                    //Jetpack*
+  remove_menu_page('edit.php');                   //Posts
+  remove_menu_page('edit.php?post_type=page');    //Pages
+  remove_menu_page('edit-comments.php');          //Comments
+  remove_menu_page('themes.php');                 //Appearance
+  remove_menu_page('plugins.php');                //Plugins
+  remove_menu_page('users.php');                  //Users
+  remove_menu_page('tools.php');                  //Tools
+  remove_menu_page('options-general.php');        //Settings
+}
+add_action('admin_menu', 'wpdocs_remove_menus');
