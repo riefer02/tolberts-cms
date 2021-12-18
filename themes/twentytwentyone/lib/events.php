a4c71d95821bfbfdb4cfb3574b3d9c580b326810
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-18 11:01:01 -0600

    comment out code

--- themes/twentytwentyone/lib/events.php
+++ themes/twentytwentyone/lib/events.php
@@ -3,9 +3,9 @@
  * Remove Default Editor for Event Post Type
  *
  */
-function wlRemoveEditorFromEvents() {
-    $post_type = 'event';
-    remove_post_type_support($post_type, 'editor');
-}
+// function wlRemoveEditorFromEvents() {
+//     $post_type = 'event';
+//     remove_post_type_support($post_type, 'editor');
+// }
 
-add_action('init', 'wlRemoveEditorFromEvents', 100);
+// add_action('init', 'wlRemoveEditorFromEvents', 100);
