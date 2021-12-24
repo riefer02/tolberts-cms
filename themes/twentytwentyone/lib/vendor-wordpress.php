324dd1aa12114eadb16403a90946185123ca9b12
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-24 14:41:41 -0600

    readd users & plugins to wp-admin

--- themes/twentytwentyone/lib/vendor-wordpress.php
+++ themes/twentytwentyone/lib/vendor-wordpress.php
@@ -9,8 +9,8 @@ function wpdocs_remove_menus() {
   remove_menu_page('edit.php?post_type=page');    //Pages
   remove_menu_page('edit-comments.php');          //Comments
   remove_menu_page('themes.php');                 //Appearance
-  remove_menu_page('plugins.php');                //Plugins
-  remove_menu_page('users.php');                  //Users
+  // remove_menu_page('plugins.php');                //Plugins
+  // remove_menu_page('users.php');                  //Users
   remove_menu_page('tools.php');                  //Tools
   remove_menu_page('options-general.php');        //Settings
 }
