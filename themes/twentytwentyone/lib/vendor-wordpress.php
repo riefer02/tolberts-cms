80dbcfe9f44ffc059230fcfd2ca6c17e779214c5
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-24 15:36:13 -0600

    test cron function

--- themes/twentytwentyone/lib/vendor-wordpress.php
+++ themes/twentytwentyone/lib/vendor-wordpress.php
@@ -6,7 +6,7 @@ function wpdocs_remove_menus() {
     remove_menu_page('index.php');                  //Dashboard
   remove_menu_page('jetpack');                    //Jetpack*
   remove_menu_page('edit.php');                   //Posts
-  remove_menu_page('edit.php?post_type=page');    //Pages
+  // remove_menu_page('edit.php?post_type=page');    //Pages
   remove_menu_page('edit-comments.php');          //Comments
   remove_menu_page('themes.php');                 //Appearance
   // remove_menu_page('plugins.php');                //Plugins
